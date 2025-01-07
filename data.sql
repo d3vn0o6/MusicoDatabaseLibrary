5e2c3d73e3ba8743f01cfef51610b8bb0ce19755
d3vn0o6 <d3vn0o6@proton.me>
2025-01-07 10:00:33 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,9 +1,1 @@
 
---Tables Overview
-	1.	Users: To store user information and preferences.
-	2.	Artists: To store artist details.
-	3.	Styles: To store musical styles/genres.
-	4.	Albums: To store album information.
-	5.	Songs: To store details about songs, including their relation to albums, artists, and styles.
-	6.	Favorites: To track users’ favorite songs or albums.
-	7.	Playlists: To allow users to create and manage custom playlists.
