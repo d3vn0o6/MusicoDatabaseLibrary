1cf8727293b0c874dd018467baa2a4350eac4689
d3vn0o6 <d3vn0o6@proton.me>
2025-01-14 01:54:18 +0100

    Update func.py

--- func.py
+++ func.py
@@ -1,9 +1,21 @@
-import sqlite3 
+import sqlite3 as db
 
 # Connect to Sqlite database 
-
-def database(db_file_path='musicodb.db'):
-  return sqlite3.connect()
+class Database:
+  def __init__(self):
+    self.data_file = "database.db"
+    self.connect()
+    self.cursor()
+    
+  def connect(self):
+    return db.connect(self.data_file)
+    
+  def cursor(self):
+    return self.connect().cursor()
+    
+    
+  
+         
   
     
 
